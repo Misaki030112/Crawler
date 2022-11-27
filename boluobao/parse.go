ef8aed92f8f2d7301a439f5456c9d9eaf703878e
WSYwsy22 <2914883754@qq.com>
2022-11-28 00:50:32 +0800

    test file download

--- boluobao/parse.go
+++ boluobao/parse.go
@@ -1,8 +1,14 @@
 package boluobao
 
-import "github.com/gocolly/colly/v2"
+import (
+	"github.com/gocolly/colly/v2"
+)
 
-// Parse the basic information of the book details page
+// BookDetailParse Parse the basic information of the book details page
 func BookDetailParse(html *colly.HTMLElement) *Book {
-
+	book := &Book{}
+	dom := html.DOM
+	book.BookName = dom.Find(".title").First().Text()
+	dom.Find(".summary-pic").First().Attr("src")
+	dom.Find("")
 }
